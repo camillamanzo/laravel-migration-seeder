21f8f1490c69218aab55324c5ee0322f9e6f6063
camillamanzo <camillasouthafrica@gmail.com>
2021-11-12 15:29:33 +0100

    added columns to holiday_packages table

---
+++ database/migrations/2021_11_12_142141_add_description_and_img__and_country_to_holiday_packages.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDescriptionAndImgAndCountryToHolidayPackages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('holiday_packages', function (Blueprint $table) {
+            $table->text('img_url');
+            $table->char('country');
+            $table->text('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('holiday_packages', function (Blueprint $table) {
+            $table->dropColumn(['img_url', 'country', 'description']);
+        });
+    }
+}
